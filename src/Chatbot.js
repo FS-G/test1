d45c7992937580161022e8b9dbe3b548c913066b
farhan <farhan.sid1111@gmail.com>
2024-10-16 04:03:23 +0500

    change of text

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -39,7 +39,7 @@ const Chatbot = () => {
     <div className="chatbot-wrapper">
       <div className="chatbot-header">
         <h1>change 1</h1>
-        <h2>Lets learn Git with Noor</h2>
+        <h2>Lets learn Git - main</h2>
       </div>
       <div className="chat-window">
         <div className="chat-messages">
